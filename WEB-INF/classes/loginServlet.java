6c2ac4d517dd76b017508a844f2e2a9c8344967a
cuizhen <songsongdahu233@gmail.com>
2015-04-30 21:30:40 +0900

    0.2.2 20150430 update

--- WEB-INF/classes/loginServlet.java
+++ WEB-INF/classes/loginServlet.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.*;
 
 public class loginServlet extends HttpServlet {
     public void doPost(HttpServletRequest request,
@@ -13,23 +14,36 @@ public class loginServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         //test
         out.println("<html>");
-        out.println("user_id:"+user_id+"password:"+password);
-        out.println("</html>");
+        out.println("user_id:"+user_id+"<br>"+"password:"+password+"<br>");
 
-        Connection conn;
-        Statement stat;
-        ResultSet rs;
+        String user_name = "";
+
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/jinanluxi?user=root&password=root");
-            try {
-                stat = conn.createStatement();
-                rs = stat.executeQuery("select * from user where user_id='"+user_id+"' and password='"+password+"'");
-            }
-            finally {
-                conn.close();
-            }
-        } catch (SQLException ex) {
+            // JDBCドライバの登録
+            String driver   = "org.gjt.mm.mysql.Driver";
+            // データベースの指定
+            String server   = "localhost";      // MySQLサーバ ( IP または ホスト名 )
+            String dbname   = "jinanluxi";         // データベース名
+            String url = "jdbc:mysql://" + server + "/" + dbname + "?useUnicode=true&characterEncoding=EUC_JP";
+            String user     = "root";         // データベース作成ユーザ名
+            String pw = "root";     // データベース作成ユーザパスワード
+            Class.forName (driver);
+            // データベースとの接続
+            Connection conn = DriverManager.getConnection(url, user, pw);
+            Statement stat = conn.createStatement();
+            String sql = "select * from user where user_id='"+user_id+"' and password='"+password+"'";
+            out.println("sql:"+sql);
+            ResultSet rs = stat.executeQuery(sql);
+            rs.next();
+            user_name = rs.getString("user_name");
+            conn.close();
+        } catch (SQLException e1) {
+
+        } catch (ClassNotFoundException e2){
 
         }
+        out.println("user_name:"+user_name+"<br>");
+        out.println("</html>");
+
     }
 }
